102cc17355438d433ab4f0b9a92c5f5c59c82a6b
Dmitry Volchek <vdn@tut.by>
2022-05-11 16:22:30 +0300

    feat: time stamp add "16:22:30_11-05-2022"

---
+++ cssMemSlider/script.js
@@ -0,0 +1,20 @@
+
+const WRAPPER = document.querySelector('.wrapper');
+
+const BACK = document.createElement('div');
+
+const IMAGE_WRAP = document.createElement ('div');
+
+const IMG = document.createElement('img');  
+
+const TEXT_AND_BUTONS_WRAP = createElement ('div');
+
+const TEXT_WRAP = document.createElement('div');
+const TEXT = document.createElement('div');
+const BUTTONS_WRAP = document.createElement('div');
+const BUTTON =document.createElement('div');
+
+
+console.log(WRAPPER)
+
+
